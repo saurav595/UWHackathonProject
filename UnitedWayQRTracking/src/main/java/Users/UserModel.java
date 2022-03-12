27066def7f69bf105d8833599affc417049780e9
Saurav Shah <shah.saur@northeastern.edu>
2022-03-12 11:37:08 -0800

    Initial Commit
    Initial Commit with Springboot

---
+++ UnitedWayQRTracking/src/main/java/Users/UserModel.java
@@ -0,0 +1,12 @@
+package Users;
+
+import java.time.LocalDate;
+
+public class UserModel {
+    private Long userID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDate dob;
+    private Long phoneNumber;
+}
---
+++ UnitedWayQRTracking/src/main/java/com/uw/UnitedWayQRTracking/UnitedWayQrTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.uw.UnitedWayQRTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitedWayQrTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitedWayQrTrackingApplication.class, args);
+	}
+
+}
